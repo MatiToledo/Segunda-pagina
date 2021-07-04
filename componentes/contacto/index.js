31d8b370f8a42b20f18e73802bd4abcbd3cbd84b
Matias Toledo <toledo.nicolas.matias@gmail.com>
2021-07-04 19:01:01 -0300

    Mandar mail

--- componentes/contacto/index.js
+++ componentes/contacto/index.js
@@ -6,15 +6,15 @@ function contactComponent(el) {
     <div class="contact__container">
       <div class="fieldset">
         <label for="name" class="label">NOMBRE</label>
-        <input type="text" class="input" />
+        <input type="text" class="input" id="name" />
       </div>
       <div class="fieldset">
         <label for="email" class="label">EMAIL</label>
-        <input type="text" class="input" />
+        <input type="text" class="input" id="email" />
       </div>
       <div class="fieldset">
         <label for="message" class="label">MENSAJE</label>
-        <textarea name="message" class="textarea"></textarea>
+        <textarea name="message" class="textarea" id="message" ></textarea>
       </div>
       <button class="contact__button">Enviar</button>
     </div>
@@ -22,4 +22,39 @@ function contactComponent(el) {
   `;
 
   el.appendChild(contactEl);
+}
+
+function sendMail(contactEl) {
+  const formEl = contactEl.querySelector(".contact__container");
+
+  formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const dataObject = Object.fromEntries(formData.entries());
+
+    let message = dataObject.name + " te acaba de enviar un mensaje!\n\n";
+    message += "> Te quiere decir esto: " + dataObject.message;
+    message += "\n\n> Podés contactarte acá: " + dataObject.email;
+
+    fetch("https://apx-api.vercel.app/api/utils/dwf", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        to: "toledo.nicolas.matias@gmail.com",
+        message: message,
+      }),
+    })
+      .then(() => {
+        alert("El mensaje fue enviado con éxito!");
+
+        contactEl.querySelectorAll(".contact-form__input").forEach((input) => {
+          input.value = "";
+        });
+        contactEl.querySelector(".contact-form__textarea").value = "";
+      })
+      .catch(() => {
+        alert("Algo salió mal al enviar el mensaje...");
+      });
+  });
 }
